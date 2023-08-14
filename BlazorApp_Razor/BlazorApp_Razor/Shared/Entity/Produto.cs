9f13dd789195dd95870e71eab963c03a370b7215
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-08-14 20:05:40 -0300

    Criação da classe produto no blazzor

---
+++ BlazorApp_Razor/BlazorApp_Razor/Shared/Entity/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorApp_Razor.Shared.Entity
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
