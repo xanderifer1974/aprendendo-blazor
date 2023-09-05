f4096057a8e43aaace00fcce09f253b6113dbe50
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-05 17:41:44 -0300

    Criando o code-behind associando a um componente blazzor.

---
+++ Blazor_ciclo_de_vida/BlazzorApp_Injecao_Dependencia/Pages/Counter.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazzorApp_Injecao_Dependencia.Servicos;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazzorApp_Injecao_Dependencia.Pages
+{
+    public class CounterBase: ComponentBase
+    {
+        protected int currentCount = 0;
+
+       [Inject] protected ServicoSingleton singleton { get; set; }
+       [Inject] protected ServicoScoped scoped { get; set; }
+       [Inject] protected ServicoTransient transient { get; set; }
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+            singleton.Valor = currentCount;
+            scoped.Valor = currentCount;
+            transient.Valor = currentCount;
+        }
+    }
+}
