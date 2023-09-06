95a7701d3821ffa470fcf464f6dffb5d1ae70703
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-06 18:06:45 -0300

    Cascating parameters class - Parâmetro em cascata com classe

---
+++ Blazor_ciclo_de_vida/BlazorCicloDeVida/Util/PessoaConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCicloDeVida.Util
+{
+    public class PessoaConfig
+    {
+        public string Nome { get; set; } = "João da Silva Soares";
+        public string Rua { get; set; } = "Rua das Flores, 35";
+        public string Bairro { get; set; } = "Jovelandia";
+        public string Cidade { get; set; } = "Atunópolis";
+        public string Estado { get; set; } = "Acre";
+    }
+}
