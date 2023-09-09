255370dcb21c59b723b594c26476440c833a8328
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-08 22:35:57 -0300

    Armazenando dados no localStorage

---
+++ 23_Blazor_LocalStorage/Blazor_LocalStorage/Blazor_LocalStorage/Startup.cs
@@ -0,0 +1,19 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor_LocalStorage
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBlazoredLocalStorage();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
