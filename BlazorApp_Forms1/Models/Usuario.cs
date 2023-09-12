ce94440f0d30cf6beb19bbb564a17811810f8ba4
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-12 17:34:26 -0300

    80. Apresentando Formulários

---
+++ BlazorApp_Forms1/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp_Forms1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
