2db4d93ac4ebbe574761c731c5896e541e9e198a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-12 20:18:06 -0300

    Sumary validation

--- BlazorApp_Forms1/Models/Usuario.cs
+++ BlazorApp_Forms1/Models/Usuario.cs
@@ -5,9 +5,11 @@ namespace BlazorApp_Forms1.Models
     public class Usuario
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="O nome deve ser informado.")]
+        [StringLength(20, ErrorMessage ="O nome não pode exceder 20 caracteres.")]
         public string? Nome { get; set; }
-        [EmailAddress]
+        [Required(ErrorMessage = "O e-mail deve ser informado.")]
+        [EmailAddress(ErrorMessage ="Informe um e-mail válido.")]
         public string? Email { get; set; }
 
         [Range(18, 80, ErrorMessage ="A idade deve estar entre 18 e 80 anos")]
