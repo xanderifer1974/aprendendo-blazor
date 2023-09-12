65b8bb391b0d40db5fb378dd32b3d7f4070669ef
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-12 19:54:12 -0300

    82. Validação de dados em Formulários

--- BlazorApp_Forms1/Models/Usuario.cs
+++ BlazorApp_Forms1/Models/Usuario.cs
@@ -1,10 +1,16 @@
-﻿namespace BlazorApp_Forms1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_Forms1.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+        [Required]
         public string? Nome { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Range(18, 80, ErrorMessage ="A idade deve estar entre 18 e 80 anos")]
         public int Idade { get; set; }
     }
 }
