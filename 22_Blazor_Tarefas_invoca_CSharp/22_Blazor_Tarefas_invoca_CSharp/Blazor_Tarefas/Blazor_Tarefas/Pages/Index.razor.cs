9ebcdeeed53cc12df26efc1eb99078de1cf017fa
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-20 14:45:01 -0300

    ajuste na index blazor

---
+++ 22_Blazor_Tarefas_invoca_CSharp/22_Blazor_Tarefas_invoca_CSharp/Blazor_Tarefas/Blazor_Tarefas/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Blazor_Tarefas.Entidades;
+using System.Collections.Generic;
+
+namespace Blazor_Tarefas.Pages
+{
+    public partial class Index
+    {
+        List<Tarefa> tarefas = new List<Tarefa>();
+
+        protected override void OnInitialized()
+        {
+            tarefas = repositorio.ObterTarefas();
+        }
+
+        private List<Tarefa> GetTarefas()
+        {
+            return tarefas;
+        }
+    }
+}
