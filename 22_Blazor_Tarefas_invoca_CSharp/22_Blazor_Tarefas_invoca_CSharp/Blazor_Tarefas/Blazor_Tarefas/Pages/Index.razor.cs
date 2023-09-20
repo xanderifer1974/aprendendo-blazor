cfae48d045e3ed95b92e42bd92b05c026e23b676
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-20 14:51:54 -0300

    Melhorias no inject na index

--- 22_Blazor_Tarefas_invoca_CSharp/22_Blazor_Tarefas_invoca_CSharp/Blazor_Tarefas/Blazor_Tarefas/Pages/Index.razor.cs
+++ 22_Blazor_Tarefas_invoca_CSharp/22_Blazor_Tarefas_invoca_CSharp/Blazor_Tarefas/Blazor_Tarefas/Pages/Index.razor.cs
@@ -1,11 +1,19 @@
 ﻿using Blazor_Tarefas.Entidades;
+using Blazor_Tarefas.Repositorios;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System.Collections.Generic;
 
 namespace Blazor_Tarefas.Pages
 {
     public partial class Index
     {
-        List<Tarefa> tarefas = new List<Tarefa>();
+        [Inject]
+        protected IRepositorio repositorio {get; set;}
+        [Inject]
+        protected IJSRuntime js { get; set;}
+
+        private List<Tarefa> tarefas = new List<Tarefa>();
 
         protected override void OnInitialized()
         {
